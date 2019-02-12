51e34ec4ee931a151f215399e2a2aab7b1e1cb58
t00137667 <t00137667@gmail.com>
2019-02-12 10:36:47 +0000

    Updated

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -14,4 +14,6 @@ public class MyArrays {
 
         return currentSize;
     }
+
+
 }
--- src/MyArraysDriver.java
+++ src/MyArraysDriver.java
@@ -1,11 +1,18 @@
 public class MyArraysDriver {
     public static void main(String[] args) {
+        int currentSize = 6;
         int[] values = new int[10];
-        values[0] = 34;
-        values[1] = 34;
-        values[2] = 34;
-
-        MyArrays.insert(values,3,2,99);
+        values[0] = 5;
+        values[1] = 27;
+        values[2] = 56;
+        values[3] = 35;
+        values[4] = 12;
+        values[5] = 67;
+        currentSize = MyArrays.insert(values,currentSize,2,99);
+        currentSize = MyArrays.insert(values,currentSize,6,75);
+        currentSize = MyArrays.insert(values,currentSize,5,80);
+        currentSize = MyArrays.insert(values,currentSize,2,23);
+        currentSize = MyArrays.insert(values,currentSize,2,50);
 
         System.out.println("Array Contents");
 
